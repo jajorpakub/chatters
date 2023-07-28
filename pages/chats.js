e1ade2411a50e1d210f04e5dda4c628755f3bb9c
jajorpakub <140837432+jajorpakub@users.noreply.github.com>
2023-07-28 18:07:08 +0200

    Initial commit

---
+++ pages/chats.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect, useContext } from "react";
+
+import { Context } from "../context";
+
+import { useRouter } from "next/router";
+import dynamic from "next/dynamic";
+
+const ChatEngine = dynamic(() =>
+  import("react-chat-engine").then((module) => module.ChatEngine)
+);
+const MessageFromSocial = dynamic(() =>
+  import("react-chat-engine").then((module) => module.MessageFromSocial)
+);
+
+export default function Chats() {
+  const { username, secret } = useContext(Context);
+  const [showChat, setShowChat] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (typeof document !== null) {
+      setShowChat(true);
+    }
+  });
+
+  useEffect(() => {
+    if (username.length === 0 || secret.length === 0) router.push("/");
+  })
+
+  if (!showChat) return  <div />;
+  return( 
+  <div className="background">
+    <div className='engine'>
+      <ChatEngine
+      height="calc(100vh - 200px)"
+      projectID="3bcf2221-a3b7-4338-a4ec-0bcad6cf8c9e"
+      userName={username}
+      userSecret={secret}
+      renderNewMessageForm={() => <MessageFromSocial />}
+      />
+    </div>
+
+  </div>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,57 @@
+import React, { useContext } from "react";
+
+import { Context } from '../context'
+
+import { useRouter } from "next/router";
+
+import axios from 'axios';
+
+export default function Auth() {
+  const { username, setUsername, secret, setSecret } = useContext(Context);
+
+  const router = useRouter()
+
+  function onSubmit(e) {
+    e.preventDefault()
+
+    if (username.length === 0 || secret.length === 0) return
+
+    axios.put(
+      'https://api.chatengine.io/users/',
+      {username, secret},
+      {headers: {"Private-key": '97dc7c0b-c99c-4669-8e35-01ac6de3053f'}}
+    )
+    .then((r) => router.push('/chats'));
+  }
+  return (
+  <div className="background">
+    <div className='auth-container'>
+      <form className='auth-form' onSubmit={e => onSubmit(e)}>
+        <div className='auth-title'>Chatters</div>
+
+        <div className='input-container'>
+          <input
+          placeholder='Podaj majla'
+          className='text-input'
+          onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className='input-container'>
+          <input
+          type='password'
+          placeholder='hasło skurwysynu'
+          className='text-input'
+          onChange={(e) => setSecret(e.target.value)}
+          />
+        </div>
+
+        <button
+          type='submit'
+          className='submit-button'>
+          Zaloguj się
+        </button>
+      </form>
+    </div>
+  </div>
+  )
+}
